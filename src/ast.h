0028a66e0236d198f3bf76413a7e9ce77dae03fe
Milan Vrbas <milan.vrbas1@gmail.com>
2023-10-29 22:54:36 +0100

    upravy

--- src/ast.h
+++ src/ast.h
@@ -8,15 +8,12 @@
 typedef enum {
     INTEGER_TYPE_EXPR,      
     DOUBLE_TYPE_EXPR,       
-    STRING_TYPE_EXPR,       
-    INTEGER_EXPR,           
-    DOUBLE_EXPR,            
-    STRING_EXPR,            
+    STRING_TYPE_EXPR,                
     VARIABLE_EXPR,          
     BINARY_OP_EXPR,         
     ASSIGN_EXPR,            
-    FUNCION_CALL_EXPR,      
-    FUNCION_DEF_EXPR,       
+    FUNCTION_CALL_EXPR,      
+    FUNCTION_DEF_EXPR,       
     RETURN_EXPR,            
     CONDITIONS_EXPR,        
     WHILE_EXPR,             
@@ -25,78 +22,117 @@ typedef enum {
     FUNCTION_PARAM_EXPR,    
     FUNCTION_ARG_EXPR,      
     BODY_EXPR,
+    VARIABLE_VALUE,
 } NodeExprType;
+
+typedef struct {
+    char* operator_expr;
+    struct Expressions* left;
+    struct Expressions* right;
+} OperatorExpr;
+
+typedef struct {
+    char* assign_expr;
+    struct Expressions* left;
+    struct Expressions* right;
+} AssignExpr;
+
+typedef struct {
+    char* func_call_name_expr;
+    struct Expression_list* func_arguments;
+} FunctionCallExpr;
+
+typedef struct {
+    char* func_def_name_expr;
+    struct Expression_list* func_parameters;
+    struct Expressions* func_body;
+} FunctionDefExpr;
+
+typedef struct {
+    struct Expressions* return_expr;
+} ReturnExpr;
+
+typedef struct {
+    struct Expressions* if_condition;
+    struct Expressions* if_body;
+    struct Expressions* else_body;
+} ConditionsExpr;
+
+typedef struct {
+    struct Expressions* while_condition;
+    struct Expressions* while_body;
+} WhileExpr;
+
+typedef struct {
+    char* var_def_name_expr;
+    struct Expressions* variable_type;
+    struct Expressions* variable_value;
+} VariableDefExpr;
+
+typedef struct {
+    char* var_dec_name_expr;
+    struct Expressions* variable_type;
+    struct Expressions* variable_value;
+} VariableDecExpr;
+
+typedef struct {
+    struct Expressions* arg_value;
+    //struct ArgumentExpr* next;
+} ArgumentExpr;
+
+typedef struct {
+    struct Expressions* param_name;
+    struct Expressions* param_type;
+    //struct ParameterExpr* next;
+} ParameterExpr;
+
+typedef struct {
+    struct Expressions* body;
+    struct AbstractSyntaxTree* next;
+} BodyExpr;
+
+typedef enum {
+    INT_VALUE,
+    DOUBLE_VALUE,
+    STRING_VALUE
+} ValueType;
+
+typedef struct {
+    ValueType type;
+    union {
+        int int_value;
+        double double_value;
+        char* string_value;
+    } value;
+} VariableValue;
 
 typedef struct Expressions {
     NodeExprType expression_type;
     union {
         struct Expressions* integer_type;   
         struct Expressions* double_type;    
-        struct Expressions* string_type;    
-        int integer_expr;                   
-        double double_expr;                 
-        char* string_expr;                  
+        struct Expressions* string_type;                    
         char* variable_expr;                
-        struct {
-            char* operator_expr;
-            struct Expressions* left;
-            struct Expressions* right;
-        } OperatorExpr;                     
-        struct {
-            char* assign_expr;
-            struct Expressions* left;
-            struct Expressions* right;
-        } AssignExpr;                       
-        struct {
-            char* func_call_name_expr;
-            struct Expression_list* func_arguments;
-        } FunctionCallExpr;                  
-        struct {
-            char* func_def_name_expr;
-            struct Expression_list* func_parameters;
-            struct Expressions* func_body;
-        } FunctionDefExpr;                   
-        struct Expressions* return_expr;     
-        struct {
-            struct Expressions* if_condition;
-            struct Expressions* if_body;
-            struct Expressions* else_body;
-        } ConditionsExpr;                    
-        struct {
-            struct Expressions* while_condition;
-            struct Expressions* while_body;
-        } WhileExpr;                         
-        struct {
-            char* var_def_name_expr;
-            struct Expressions* varible_type;
-            struct Expressions* variable_value;
-        } VariableDefExpr;                   
-        struct {
-            char* var_dec_name_expr;
-            struct Expressions* varible_type;
-            struct Expressions* variable_value;
-        } VariableDecExpr;                   
-        
-        struct {
-            struct Expressions* arg_value;
-            //struct ArgumentExpr* next;
-        } ArgumentExpr;                       
-        struct {
-            struct Expressions* param_name;
-            struct Expressions* param_type;
-            //struct ParameterExpr* next;
-        } ParameterExpr;                       
-        struct {
-            struct Expressions* body; 
-            struct Expression_list* next;
-        } BodyExpr;
+        OperatorExpr operator_expr;        
+        AssignExpr assign_expr;            
+        FunctionCallExpr* func_call_expr;   
+        FunctionDefExpr* func_def_expr;     
+        ReturnExpr* return_expr;            
+        ConditionsExpr* conditions_expr;   
+        WhileExpr* while_expr;              
+        VariableDefExpr* var_def_expr;      
+        VariableDecExpr* var_dec_expr;      
+        ArgumentExpr* argument_expr;        
+        ParameterExpr* parameter_expr;     
+        BodyExpr* body_expr;               
+        VariableValue variable_value; 
     } ExpressionUnion;
-} AbstractSyntaxTree;
+} AbstractSyntaxTree;   
 
-    typedef struct Expression_list {
-        AbstractSyntaxTree node;
-        struct Expression_list* next;
-    } ASTList;
+typedef struct Expression_list {
+    AbstractSyntaxTree node;
+    struct Expression_list* next;
+} ASTList;
 
 void free_ast(AbstractSyntaxTree* node);
 
@@ -104,7 +140,7 @@ void free_ast_list(ASTList* list);
 
 void preorder_traversal(AbstractSyntaxTree* node);
 
-AbstractSyntaxTree* node_fail(AbstractSyntaxTree* node);
+void node_fail(AbstractSyntaxTree* node);
 
 AbstractSyntaxTree* make_binaryExpr(char* operator_expr, AbstractSyntaxTree* left, AbstractSyntaxTree* right);
 
@@ -140,6 +176,6 @@ AbstractSyntaxTree* make_ArgumentExpr(AbstractSyntaxTree* arg_name);
 
 AbstractSyntaxTree* make_ParameterExpr(AbstractSyntaxTree* param_name, AbstractSyntaxTree* param_type);
 
-AbstractSyntaxTree* make_BodyExpr(AbstractSyntaxTree* body, ASTList* next);
+AbstractSyntaxTree* make_BodyExpr(AbstractSyntaxTree* body, AbstractSyntaxTree* next);
 
 #endif
