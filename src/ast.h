6f7f517ce339b089f58136f8633cb8bb22692643
Dandys <tomasdaniel7800@gmail.com>
2023-11-04 19:27:07 +0100

    Refactor AST and support multiline bodies as well as multiple function params and so

--- src/ast.h
+++ src/ast.h
@@ -1,181 +1,128 @@
-#ifndef AST
-#define AST
+#ifndef AST_H_INCLUDED
+#define AST_H_INCLUDED
 
 #include <stdio.h>
 #include <stdlib.h>
 #include "utils.h"
+#include "vec.h"
+#include "lexer.h"
 
-typedef enum {
-    INTEGER_TYPE_EXPR,      
-    DOUBLE_TYPE_EXPR,       
-    STRING_TYPE_EXPR,                
-    VARIABLE_EXPR,          
-    BINARY_OP_EXPR,         
-    ASSIGN_EXPR,            
-    FUNCTION_CALL_EXPR,      
-    FUNCTION_DEF_EXPR,       
-    RETURN_EXPR,            
-    CONDITIONS_EXPR,        
-    WHILE_EXPR,             
-    VARIABLE_DEF_EXPR,      
-    VARIABLE_DEC_EXPR,      
-    FUNCTION_PARAM_EXPR,    
-    FUNCTION_ARG_EXPR,      
-    BODY_EXPR,
+enum NodeExprType{
+    BINARY_OP_EXPR,
+    FUNCTION_CALL_EXPR,
+    FUNCTION_DEF_EXPR,
+    RETURN_EXPR,
+    CONDITIONS_EXPR,
+    WHILE_EXPR,
+    VARIABLE_DEC_EXPR,
+    FUNCTION_PARAM_EXPR,
     VARIABLE_VALUE,
-} NodeExprType;
+};
+
+// Forward declarations
+typedef struct AbstractSyntaxTree AbstractSyntaxTree;
+typedef struct VariableValue VariableValue;
 
 typedef struct {
-    char* operator_expr;
-    struct Expressions* left;
-    struct Expressions* right;
+    String operator_expr;
+    AbstractSyntaxTree* left;
+    AbstractSyntaxTree* right;
 } OperatorExpr;
 
 typedef struct {
-    char* assign_expr;
-    struct Expressions* left;
-    struct Expressions* right;
-} AssignExpr;
-
-typedef struct {
-    char* func_call_name_expr;
-    struct Expression_list* func_arguments;
+    String func_name;
+    Vec* func_arguments;
 } FunctionCallExpr;
 
+enum ValueType {
+    INT_VALUE,
+    DOUBLE_VALUE,
+    STRING_VALUE,
+    UNKNOWN // Value will be determined from symtable
+};
+
 typedef struct {
-    char* func_def_name_expr;
-    struct Expression_list* func_parameters;
-    struct Expressions* func_body;
+    String func_name;
+    Vec* func_parameters;
+    Vec* func_body;
+    enum ValueType ret_val;
 } FunctionDefExpr;
 
 typedef struct {
-    struct Expressions* return_expr;
+    AbstractSyntaxTree* return_expr;
 } ReturnExpr;
 
 typedef struct {
-    struct Expressions* if_condition;
-    struct Expressions* if_body;
-    struct Expressions* else_body;
+    AbstractSyntaxTree* if_condition;
+    Vec* if_body;
+    Vec* else_body;
 } ConditionsExpr;
 
 typedef struct {
-    struct Expressions* while_condition;
-    struct Expressions* while_body;
+    AbstractSyntaxTree* while_condition;
+    Vec* while_body;
 } WhileExpr;
 
-typedef struct {
-    char* var_def_name_expr;
-    struct Expressions* variable_type;
-    struct Expressions* variable_value;
-} VariableDefExpr;
-
-typedef struct {
-    char* var_dec_name_expr;
-    struct Expressions* variable_type;
-    struct Expressions* variable_value;
-} VariableDecExpr;
-
-typedef struct {
-    struct Expressions* arg_value;
-    struct Expression_list* next;
-} ArgumentExpr;
-
-typedef struct {
-    struct Expressions* param_name;
-    struct Expressions* param_type;
-    struct Expression_list* next;
-} ParameterExpr;
-
-typedef struct {
-    struct Expressions* body;
-    struct Expression_list* next;
-} BodyExpr;
-
-typedef enum {
-    INT_VALUE,
-    DOUBLE_VALUE,
-    STRING_VALUE
-} ValueType;
-
-typedef struct {
-    ValueType type;
+typedef struct VariableValue {
+    enum ValueType type;
     union {
         int int_value;
         double double_value;
-        char* string_value;
-    } value;
+        String string_value;
+    } ValueUnion;
 } VariableValue;
 
-typedef struct Expressions {
-    NodeExprType expression_type;
+typedef struct {
+    String name;
     union {
-        struct Expressions* integer_type;   
-        struct Expressions* double_type;    
-        struct Expressions* string_type;                    
-        char* variable_expr;                
-        OperatorExpr operator_expr;        
-        AssignExpr assign_expr;            
-        FunctionCallExpr func_call_expr;   
-        FunctionDefExpr func_def_expr;     
-        ReturnExpr return_expr;            
-        ConditionsExpr conditions_expr;   
-        WhileExpr while_expr;              
-        VariableDefExpr var_def_expr;      
-        VariableDecExpr var_dec_expr;      
-        ArgumentExpr argument_expr;        
-        ParameterExpr parameter_expr;     
-        BodyExpr body_expr;               
-        VariableValue variable_value; 
-    } ExpressionUnion;
-} AbstractSyntaxTree;   
+        VariableValue value;
+        AbstractSyntaxTree* right_part;
+    } VarUnion;
+} VariableExpr;
 
-typedef struct Expression_list {
-    AbstractSyntaxTree node;
-    struct Expression_list* next;
-} ASTList;
+typedef struct {
+    String param_name;
+    String param_ident;
+    enum ValueType param_type;
+} ParameterExpr;
+
+typedef struct AbstractSyntaxTree {
+    enum NodeExprType expression_type;
+    union {
+        OperatorExpr        operator_expr;
+        FunctionCallExpr    func_call_expr;
+        FunctionDefExpr     func_def_expr;
+        ReturnExpr          return_expr;
+        ConditionsExpr      conditions_expr;
+        WhileExpr           while_expr;
+        VariableExpr        var_expr;
+        ParameterExpr       parameter_expr;
+        VariableValue       variable_value;
+    } ExpressionUnion;
+} AbstractSyntaxTree;
 
 void free_ast(AbstractSyntaxTree* node);
 
-void free_ast_list(ASTList* list);
+void free_ast_vec(Vec* vec);
 
 void preorder_traversal(AbstractSyntaxTree* node);
 
-void node_fail(AbstractSyntaxTree* node);
-
-AbstractSyntaxTree* make_binaryExpr(char* operator_expr, AbstractSyntaxTree* left, AbstractSyntaxTree* right);
-
-AbstractSyntaxTree* make_variableExpr(char* variable_name);
-
-AbstractSyntaxTree* make_integerExpr(int value);
-
-AbstractSyntaxTree* make_integerType(AbstractSyntaxTree* value);
-
-AbstractSyntaxTree* make_doubleExpr(double value);
-
-AbstractSyntaxTree* make_doubleType(AbstractSyntaxTree* value);
-
-AbstractSyntaxTree* make_stringExpr(char* value);
-
-AbstractSyntaxTree* make_stringType(AbstractSyntaxTree* value);
+AbstractSyntaxTree* make_binaryExpr(String operator_expr, AbstractSyntaxTree* left, AbstractSyntaxTree* right);
 
 AbstractSyntaxTree* make_returnExpr(AbstractSyntaxTree* return_value);
 
-AbstractSyntaxTree* make_whileExpr(AbstractSyntaxTree* while_condition, AbstractSyntaxTree* while_body);
+AbstractSyntaxTree* make_whileExpr(AbstractSyntaxTree* while_condition, Vec* while_body);
 
-AbstractSyntaxTree* make_conditionExpr(AbstractSyntaxTree* if_condition, AbstractSyntaxTree* if_body, AbstractSyntaxTree* else_body);
+AbstractSyntaxTree* make_conditionExpr(AbstractSyntaxTree* if_condition, Vec* if_body, Vec* else_body);
 
-AbstractSyntaxTree* make_function_callExpr(char* func_call_name_expr, ASTList* func_arguments);
+AbstractSyntaxTree* make_function_callExpr(String func_name, Vec* func_arguments);
 
-AbstractSyntaxTree* make_function_defExpr(char* func_def_name_expr, ASTList* func_parameters, AbstractSyntaxTree* func_body);
+AbstractSyntaxTree* make_function_defExpr(String func_name, Vec* func_parameters, Vec* func_body, enum ValueType ret_val);
 
-AbstractSyntaxTree* make_variable_defExpr(char* var_def_name_expr, AbstractSyntaxTree* varible_type, AbstractSyntaxTree* variable_value);
+AbstractSyntaxTree* make_variable_Expr(String var_name, enum ValueType type, AbstractSyntaxTree* right_part);
 
-AbstractSyntaxTree* make_variable_decExpr(char* var_dec_name_expr, AbstractSyntaxTree* varible_type, AbstractSyntaxTree* variable_value);
+AbstractSyntaxTree* make_valueExpr(String arg_name, enum ValueType type, Lexer* lex);
 
-AbstractSyntaxTree* make_ArgumentExpr(AbstractSyntaxTree* arg_name);
+AbstractSyntaxTree* make_parameterExpr(String param_name, String param_ident, enum ValueType param_type);
 
-AbstractSyntaxTree* make_ParameterExpr(AbstractSyntaxTree* param_name, AbstractSyntaxTree* param_type, ASTList* next);
-
-AbstractSyntaxTree* make_BodyExpr(AbstractSyntaxTree* body, ASTList* next);
-
-#endif
+#endif // AST_H_INCLUDED
--- src/main.c
+++ src/main.c
@@ -8,6 +8,16 @@
 #include "stream.h"
 #include "symtable.h"
 #include "vec.h"
+#include "ast.h"
+
+void print_node_data(Tree *tree, NodeData **data, const char *key) {
+    *data = tree_find(tree, key);
+    if (*data)
+        // Also print data stored in found node
+        fprintf(stdout, "Node %s found, data.layer=%d\n", key, (*data)->layer);
+    else
+        fprintf(stdout, "Node %s NOT found\n", key);
+}
 
 int main(void) {
     FILE* file = fopen(DEBUG_FILE, "r");
@@ -42,6 +52,129 @@ int main(void) {
         }
         token = lex_next(&lexer);
     }
+    fclose(file);
     lex_free(&lexer);
     printf("----------------------------------------\n");
+}
+
+
+    printf("----------------------------------------\n");
+    // AST Basic Tests
+
+    // pom : String = 5
+    AbstractSyntaxTree* var_tree = make_variable_Expr(STR("pom"),
+                                                      STRING_VALUE,
+                                                      make_valueExpr(NUL_STR,
+                                                                     INT_VALUE,
+                                                                     &((Lexer) {.i_num = 5})
+                                                                    )
+                                                     );
+    preorder_traversal(var_tree);
+    free_ast(var_tree);
+
+    // pom : String = funcA(ahoj, svete);
+    Vec arg_vec = VEC_NEW(AbstractSyntaxTree*);
+    VEC_PUSH(&arg_vec, AbstractSyntaxTree*, make_valueExpr(STR("ahoj"),
+                                                           STRING_VALUE,
+                                                           &((Lexer) {.str = STR("ahoj")}) // Real value will be found in symtable
+                                                          ));
+    VEC_PUSH(&arg_vec, AbstractSyntaxTree*, make_valueExpr(STR("svete"),
+                                                           STRING_VALUE,
+                                                           &((Lexer) {.str = STR("svete")})
+                                                          ));
+
+    var_tree = make_variable_Expr(STR("pom"),
+                                  STRING_VALUE,
+                                  make_function_callExpr(STR("funcA"), &arg_vec));
+    preorder_traversal(var_tree);
+    free_ast(var_tree);
+
+    /*
+        if (a < b) {
+            a = 8;
+            b = 10 - a;
+        }
+        else {
+            return funcB(jeden);
+        }
+    */
+    Vec if_body = VEC_NEW(AbstractSyntaxTree*);
+    VEC_PUSH(&if_body, AbstractSyntaxTree*, make_variable_Expr(STR("a"),
+                                                               INT_VALUE,
+                                                               make_valueExpr(NUL_STR,
+                                                                              INT_VALUE,
+                                                                              &((Lexer) {.i_num = 8})
+                                                                             )
+                                                              ));
+    VEC_PUSH(&if_body, AbstractSyntaxTree*, make_variable_Expr(STR("b"),
+                                                               INT_VALUE,
+                                                               make_binaryExpr(STR("-"),
+                                                                               make_valueExpr(NUL_STR, INT_VALUE, &((Lexer) {.i_num = 10})),
+                                                                               make_valueExpr(NUL_STR, STRING_VALUE, &((Lexer) {.str = STR("a")})))
+                                                              ));
+
+    Vec func_arg = VEC_NEW(AbstractSyntaxTree*);
+    VEC_PUSH(&func_arg, AbstractSyntaxTree*, make_valueExpr(STR("jeden"),
+                                                            STRING_VALUE,
+                                                            &((Lexer) {.str = STR("jeden")})
+                                                           ));
+    Vec else_body = VEC_NEW(AbstractSyntaxTree*);
+    VEC_PUSH(&else_body, AbstractSyntaxTree*, make_returnExpr(make_function_callExpr(STR("funcB"), &func_arg)));
+
+    AbstractSyntaxTree* cond = make_conditionExpr(make_binaryExpr(STR("<"),
+                                                                  make_variable_Expr(STR("a"), STRING_VALUE, NULL),
+                                                                  make_variable_Expr(STR("b"), STRING_VALUE, NULL)),
+                                                  &if_body,
+                                                  &else_body);
+    preorder_traversal(cond);
+    free_ast(cond);
+
+    /*
+        while (a == b) {
+            print("Dobry den");
+        }
+    */
+    func_arg = VEC_NEW(AbstractSyntaxTree*);
+    VEC_PUSH(&func_arg, AbstractSyntaxTree*, make_valueExpr(STR("Dobry den"),
+                                                            STRING_VALUE,
+                                                            &((Lexer) {.str = STR("Dobry den")})
+                                                           ));
+    Vec while_body = VEC_NEW(AbstractSyntaxTree*);
+    VEC_PUSH(&while_body, AbstractSyntaxTree*, make_function_callExpr(STR("print"), &func_arg));
+
+    AbstractSyntaxTree* while_cyc = make_whileExpr(make_binaryExpr(STR("=="),
+                                                                   make_variable_Expr(STR("a"), STRING_VALUE, NULL),
+                                                                   make_variable_Expr(STR("b"), STRING_VALUE, NULL)),
+                                                   &while_body);
+    preorder_traversal(while_cyc);
+    free_ast(while_cyc);
+
+    /*
+        func func_def (name name_param : String, age age_param : Int) -> Int {
+            let a = "jenom jeden radek";
+        }
+    */
+    Vec params_vec = VEC_NEW(AbstractSyntaxTree*);
+    VEC_PUSH(&params_vec, AbstractSyntaxTree*, make_parameterExpr(STR("name"),
+                                                                  STR("name_param"),
+                                                                  STRING_VALUE));
+    VEC_PUSH(&params_vec, AbstractSyntaxTree*, make_parameterExpr(STR("age"),
+                                                                  STR("age_param"),
+                                                                  INT_VALUE));
+
+    Vec body_vec = VEC_NEW(AbstractSyntaxTree*);
+    VEC_PUSH(&body_vec, AbstractSyntaxTree*, make_variable_Expr(STR("a"),
+                                                                STRING_VALUE,
+                                                                make_valueExpr(STR("a"),
+                                                                               STRING_VALUE,
+                                                                               &((Lexer) {.str = STR("jenom jeden radek")}))
+                                                               ));
+
+    AbstractSyntaxTree* func = make_function_defExpr(STR("func_def"),
+                                                     &params_vec,
+                                                     &body_vec,
+                                                     INT_VALUE);
+    preorder_traversal(func);
+    free_ast(func);
+
 }
